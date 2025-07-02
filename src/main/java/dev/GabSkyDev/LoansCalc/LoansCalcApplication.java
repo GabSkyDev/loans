5cbc2edac03a2bec6444b222861ba8803550e390
Gabriel Sousa <ghostamino08@gmail.com>
2025-07-02 17:19:19 -0300

    first commit

---
+++ src/main/java/dev/GabSkyDev/LoansCalc/LoansCalcApplication.java
@@ -0,0 +1,13 @@
+package dev.GabSkyDev.LoansCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/GabSkyDev/LoansCalc/LoansCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.GabSkyDev.LoansCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
